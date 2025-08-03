4d8bd74ef2e3195316c0ff1ac235e09db7aa25f4
qianyiming <837573015@qq.com>
2025-08-03 22:06:38 +0800

    fix: fixed keyEvent not to be propagated to other key event handlers

--- lib/src/pluto_grid.dart
+++ lib/src/pluto_grid.dart
@@ -628,7 +628,7 @@ class PlutoGridState extends PlutoStateWithChange<PlutoGrid> {
       ));
     }
 
-    return _keyManager.eventResult.consume(KeyEventResult.handled);
+    return _keyManager.eventResult.consume(KeyEventResult.ignored);
   }
 
 
